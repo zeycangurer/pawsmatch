ad03ebc440a15bc8103053e699d99032dcf51f26
Betül Ceren YETİZ <betulcerenyetiz@gmail.com>
2022-12-05 21:50:01 +0300

    syntax changed

--- src/api/database.js
+++ src/api/database.js
@@ -1,26 +1,26 @@
 import database from '@react-native-firebase/database';
 
 export const addPet = async pet => {
-  const newPet = await database().ref('/pets').push();
+  const newPet = await database().ref('/Pets').push();
   newPet.set(pet);
 };
 
 export const getPets = async () => {
-  const pets = await database().ref('/pets').once('value');
+  const pets = await database().ref('/Pets').once('value');
   return pets.val();
 };
 
 export const getPet = async id => {
-  const pet = await database().ref(`/pets/${id}`).once('value');
+  const pet = await database().ref(`/Pets/${id}`).once('value');
   return pet.val();
 };
 
 export const updatePet = async (id, pet) => {
-  const updatedPet = await database().ref(`/pets/${id}`).update(pet);
+  const updatedPet = await database().ref(`/Pets/${id}`).update(pet);
   return updatedPet;
 };
 
 export const deletePet = async id => {
-  const deletedPet = await database().ref(`/pets/${id}`).remove();
+  const deletedPet = await database().ref(`/Pets/${id}`).remove();
   return deletedPet;
 };
